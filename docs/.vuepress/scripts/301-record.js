acf52b14f11cd64084a0f88be1792e1b126fb675
seognil LC <seognil@gmail.com>
2021-08-11 21:35:46 +0800

    fix: 301

--- docs/.vuepress/scripts/301-record.js
+++ docs/.vuepress/scripts/301-record.js
@@ -3,7 +3,7 @@ const urlMapping = {
     '/note/fe-development-cookbook-old',
     '/note/fe-development-cookbook',
   ],
-  '/note/js-basic': ['/note/javascript-language-basic'],
+  '/note/javascript-language-basic': ['/note/js-basic'],
 };
 
 // * ================================================================================
